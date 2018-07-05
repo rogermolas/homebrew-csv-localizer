7132faf79a38b93bb60c8f6e3ada4da6f3cd22f7
Roger Molas <r.molas06@gmail.com>
2018-07-05 21:26:46 +0800

    update formula

--- csv-localizer.rb
+++ csv-localizer.rb
@@ -1,8 +1,8 @@
 class CsvLocalizer < Formula
   desc "Convert CSV file to iOS and Android localizable strings"
   homepage ""
-  url "https://github.com/rogermolas/csv-localizer/archive/v1.0.1.tar.gz"
-  sha256 "6fbbac027036464695375d96627ce740eaf2380eba41e2f81873d340efc0d582"
+  url "https://github.com/rogermolas/csv-localizer/archive/v1.0.0.tar.gz"
+  sha256 "1d4ceef62fe4f60febdcde2b9690aff50c3fe9ac2e3c9e0cb17f80c01456214a"
   version "1.0.0"
   depends_on :python if MacOS.version <= :snow_leopard
 
