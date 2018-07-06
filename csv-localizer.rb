76663dcb5bd252f706ea5242b209bebadc73bde8
Roger Molas <r.molas06@gmail.com>
2018-07-06 14:56:10 +0800

    Remove whte space

--- csv-localizer.rb
+++ csv-localizer.rb
@@ -2,12 +2,11 @@ class CsvLocalizer < Formula
   desc "Convert CSV file to iOS and Android localizable strings"
   homepage "https://github.com/rogermolas/csv-localizer"
   url "https://github.com/rogermolas/csv-localizer/archive/v1.2.0.tar.gz"
-  version "1.2.0"
   sha256 "4e59c40f3404cc5aa5e874342b72a08ac9c2ae1270beb4430318f59ad9b929e1"
   def install
     bin.install "csv-localizer"
   end
-  
+
   test do
     system "#{bin}/csv-localizer", "-h"
   end
