d14675dd66210ebc9be951c05f0f7e3124febe35
Roger Molas <r.molas06@gmail.com>
2018-07-06 14:51:19 +0800

    Remove idention

--- csv-localizer.rb
+++ csv-localizer.rb
@@ -1,16 +1,14 @@
 class CsvLocalizer < Formula
-    desc "Convert CSV file to iOS and Android localizable strings"
-    homepage "https://github.com/rogermolas/csv-localizer"
-    url "https://github.com/rogermolas/csv-localizer/archive/v1.2.0.tar.gz"
-    sha256 "4e59c40f3404cc5aa5e874342b72a08ac9c2ae1270beb4430318f59ad9b929e1"
-    version "1.2.0"
-    depends_on :python if MacOS.version <= :snow_leopard
-    
-    def install
-        bin.install "csv-localizer"
-    end
-
-    test do
-        system "#{bin}/csv-localizer", "-h"
-    end
+  desc "Convert CSV file to iOS and Android localizable strings"
+  homepage "https://github.com/rogermolas/csv-localizer"
+  url "https://github.com/rogermolas/csv-localizer/archive/v1.2.0.tar.gz"
+  version "1.2.0"
+  sha256 "4e59c40f3404cc5aa5e874342b72a08ac9c2ae1270beb4430318f59ad9b929e1"
+  def install
+    bin.install "csv-localizer"
+  end
+  
+  test do
+    system "#{bin}/csv-localizer", "-h"
+  end
 end
