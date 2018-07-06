ace0229f103241d2969f0244138781e0e5165e22
Roger Molas <r.molas06@gmail.com>
2018-07-06 17:57:21 +0800

    Add dependency

--- csv-localizer.rb
+++ csv-localizer.rb
@@ -4,6 +4,8 @@ class CsvLocalizer < Formula
   url "https://github.com/rogermolas/csv-localizer/archive/v1.2.1.tar.gz"
   sha256 "9ed12ed932673521659bee64edb9ff727c845a3a948aac00a3354b5f96a825e8"
 
+  depends_on "python@2"
+
   def install
     bin.install "csv-localizer"
   end
