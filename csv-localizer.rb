6d667da1b490d8c8b11aab52f04b2656d88db509
Roger Molas <r.molas06@gmail.com>
2020-06-24 21:13:58 +0800

    Update tap to pyhon3

--- csv-localizer.rb
+++ csv-localizer.rb
@@ -4,7 +4,7 @@ class CsvLocalizer < Formula
   url "https://github.com/rogermolas/csv-localizer/archive/v1.2.2.tar.gz"
   sha256 "03e89e1b90b5f55b7399be869b88b39e4da3a556ce6dd89e999af188bca790d2"
 
-  depends_on "python@2"
+  depends_on "python@3"
 
   def install
     bin.install "csv-localizer"
